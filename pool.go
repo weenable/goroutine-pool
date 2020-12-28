fa2448012bb06d4ca6c86974f910b427f36f2722
eslrain <eslrain@gmail.com>
2020-12-28 12:13:52 +0800

    添加信号crod相关

--- pool.go
+++ pool.go
@@ -14,45 +14,53 @@ const (
 )
 
 type Pool struct {
-	expiryDuration time.Duration
-	logger *log.Logger
-	lock sync.Locker
-	size int32
-	workerCache sync.Pool
-	state int32
-	workers *workerStack
-	running int32
+	expiryDuration   time.Duration
+	logger           *log.Logger
+	lock             sync.Locker
+	size             int32
+	workerCache      sync.Pool
+	state            int32
+	workers          *workerStack
+	running          int32
+	cond             *sync.Cond
+	maxBlockingTasks int
+	nonblocking      bool
+	blockingNum      int
 }
 
 func (p *Pool) purgePeriodically() {
 	heatbeat := time.NewTicker(p.expiryDuration)
 	defer heatbeat.Stop()
-	
+
 	for range heatbeat.C {
 		if atomic.LoadInt32(&p.state) == CLOSED {
 			break
 		}
-		
+
 		p.lock.Lock()
 		expiredWorkers := p.workers.retrieveExpiry(p.expiryDuration)
 		p.lock.Unlock()
-		
+
 		for i := range expiredWorkers {
 			expiredWorkers[i].task <- nil
 			expiredWorkers[i] = nil
 		}
-		
+
+		if p.Running() == 0 {
+			p.cond.Broadcast()
+		}
+
 	}
 }
 
-func NewPool(size int32) (*Pool) {
+func NewPool(size int32) *Pool {
 	p := &Pool{
 		expiryDuration: 10 * time.Second, //10秒启动一次清理任务
-		logger: defaultLogger,
-		lock: internal.NewSpinLock(),
-		size: size,
+		logger:         defaultLogger,
+		lock:           internal.NewSpinLock(),
+		size:           size,
 	}
-	
+
 	//sync.pool初始化
 	p.workerCache.New = func() interface{} {
 		return &goWorker{
@@ -60,10 +68,12 @@ func NewPool(size int32) (*Pool) {
 			task: make(chan func(), 1),
 		}
 	}
-	
+
 	//活跃worker列表
 	p.workers = newWorkerStack(size)
-	
+
+	p.cond = sync.NewCond(p.lock)
+
 	//清理任务
 	go p.purgePeriodically()
 	return p
@@ -73,10 +83,10 @@ func (p *Pool) Submit(task func()) error {
 	if atomic.LoadInt32(&p.state) == CLOSED {
 		return ErrPoolClosed
 	}
-	
+
 	var w *goWorker
 	if w = p.retrieveWorker(); w == nil {
-		 return ErrPoolOverload
+		return ErrPoolOverload
 	}
 	w.task <- task
 	return nil
@@ -106,8 +116,9 @@ func (p *Pool) retrieveWorker() (w *goWorker) {
 		w = p.workerCache.Get().(*goWorker)
 		w.run()
 	}
-	
+
 	p.lock.Lock()
+	defer p.lock.Unlock()
 	w = p.workers.detach()
 	if w != nil {
 		p.lock.Unlock()
@@ -118,27 +129,44 @@ func (p *Pool) retrieveWorker() (w *goWorker) {
 	} else if p.Running() < size {
 		// 从sync.Pool中拿新的goroutine
 		spawnWorker()
+	} else {
+		if p.nonblocking {
+			return
+		}
+	Reentry:
+		p.blockingNum++
+		p.cond.Wait()
+		p.blockingNum--
+		if p.Running() == 0 {
+			p.lock.Unlock()
+			spawnWorker()
+			return
+		}
+		w = p.workers.detach()
+		if w == nil {
+			goto Reentry
+		}
 	}
 	return
 }
 
 func (p *Pool) revertWorker(worker *goWorker) bool {
-	if (p.size >0 && p.Running() > p.size){
+	if p.size > 0 && p.Running() > p.size {
 		// 不用还了，直接回收
 		return false
 	}
-	
+
 	worker.recycleTime = time.Now()
-	
+
 	//goroutine池已经关闭
 	p.lock.Lock()
 	defer p.lock.Unlock()
 	if atomic.LoadInt32(&p.state) == CLOSED {
 		return false
 	}
-	
+
 	p.workers.insert(worker)
+	p.cond.Signal()
 	return true
-	
-	
+
 }
